03041ff290346645f2b3e38cc8f4cf9a470d7deb
Aleksey2710 <Aleksey_2710@mail.ru>
2023-06-01 13:22:04 +0300

    initial commit

---
+++ src/main/java/pro/sky/adsonlineapp/AdsOnlineAppApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.adsonlineapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsOnlineAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdsOnlineAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/adsonlineapp/AdsOnlineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.adsonlineapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsOnlineAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
