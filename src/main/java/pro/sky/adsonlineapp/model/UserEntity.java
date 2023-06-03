de90b2838bdede2ee2cc38d2fed1c7338698baf3
AgentOlga <agentolga@gmail.com>
2023-06-03 19:16:46 +0200

    class UserEntity

---
+++ src/main/java/pro/sky/adsonlineapp/model/UserEntity.java
@@ -0,0 +1,24 @@
+package pro.sky.adsonlineapp.model;
+
+import pro.sky.adsonlineapp.dto.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private Role role;
+    private String image;
+}
