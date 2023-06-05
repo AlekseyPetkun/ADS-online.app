0f4a144cb37e13d489c930038a8ec6c212a86e40
AgentOlga <agentolga@gmail.com>
2023-06-05 18:44:59 +0200

    Creating UserService, UserRepository and Impl

---
+++ src/main/java/pro/sky/adsonlineapp/dto/User.java
@@ -0,0 +1,14 @@
+package pro.sky.adsonlineapp.dto;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Integer id; // id пользователя
+    private String email; // логин пользователя
+    private String firstName;// имя пользователя
+    private String lastName;// фамилия пользователя
+    private String phone; // телефон пользователя
+    private String image;// ссылка на аватар пользователя
+}
