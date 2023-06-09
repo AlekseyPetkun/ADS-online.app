babc5d2a3400a979f282a135799bd298d39b04c8
SerNike <seda.2000@inbox.ru>
2023-06-09 17:12:56 +0500

    исправил до конца

--- src/main/java/pro/sky/adsonlineapp/controllers/CommentController.java
+++ src/main/java/pro/sky/adsonlineapp/controllers/CommentController.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pro.sky.adsonlineapp.dto.CommentDto;
+import pro.sky.adsonlineapp.dto.CreateComment;
 import pro.sky.adsonlineapp.dto.ResponseWrapperComment;
 import pro.sky.adsonlineapp.model.Comment;
 
@@ -28,8 +30,8 @@ public class CommentController {
             responseCode = "401",
             description = "для того чтобы оставить комментарий необходимо авторизоваться"
     )
-    public ResponseEntity<ResponseWrapperComment> saveComment(@PathVariable("id") int id,
-                                                         @RequestBody ResponseWrapperComment comments) {
+    public ResponseEntity<?> addComment(@PathVariable("id") int id,
+                                         @RequestBody CreateComment comments) {
 
         return ResponseEntity.ok(comments);
     }
@@ -75,7 +77,7 @@ public class CommentController {
             responseCode = "403",
             description = "отстутсвуют права доступа"
     )
-    public ResponseEntity<Object> deleteComments(@PathVariable Integer pk,
+    public ResponseEntity<Object> deleteComment(@PathVariable Integer adId,
                                                  @PathVariable Integer commentId) {
 
         return ResponseEntity.ok().build();
@@ -98,8 +100,9 @@ public class CommentController {
             responseCode = "403",
             description = "отстутсвуют права доступа"
     )
-    public ResponseEntity<Comment> updateComments(@PathVariable Integer id,
-                                                  @PathVariable Integer commentId) {
+    public ResponseEntity<CommentDto> updateComment(@PathVariable Integer adId,
+                                                  @PathVariable Integer commentId,
+                                                  @RequestBody CommentDto commentDto) {
 
         return ResponseEntity.ok().build();
     }
